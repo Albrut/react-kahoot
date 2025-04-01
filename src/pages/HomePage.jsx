eb7d301a1bac9a025967767f6730a5597b8db63d
Альберт Гадиев <albertgadiev@yandex.com>
2025-04-01 22:30:17 +0600

    changed main page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,23 @@
 import { Link } from "react-router-dom";
 import "../css/homePage.css";
-import "../css/main.css"
+import "../css/main.css";
 
 export default function HomePage() {
     return (
         <div className="home-container">
             <div className="home-text-container">
-                <h1 className="home-title">Добро пожаловать!</h1>
-                <p className="home-text">Выберите действие:</p>
+                <h1 className="home-title">Добро пожаловать в GameMaster!</h1>
+                <p className="home-description">
+                    GameMaster — это интерактивная игра, где ваши знания и реакция решают всё!
+                    Сразитесь с друзьями, пройдите уникальные испытания и станьте чемпионом!
+                </p>
+                <p className="home-text">
+                        Готовы начать приключение?
+                </p>
             </div>
             <div className="home-buttons">
                 <Link to="/login" className="home-button-link">Войти</Link>
                 <Link to="/register" className="home-button-link">Регистрация</Link>
-                <Link to="/game" className="home-button-link">Играть</Link>
             </div>
         </div>
     );
